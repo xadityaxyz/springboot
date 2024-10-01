01cf865b51f12793b787b697a5b7884a6347a12c
xadityaxyz <aadiwadekar47@gmail.com>
2024-10-01 15:51:16 +0530

    aditya wadekar 01/10/2024

---
+++ src/main/java/com/example/demo/Rescontrooller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@ResponseBody
+@RestController
+public class Rescontrooller {
+	
+	@GetMapping("/home")
+	public String callingHome() {
+		
+		return "home run";
+	}
+	
+
+}
